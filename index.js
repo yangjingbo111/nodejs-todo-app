e543a23209db85313d4b275b8dd917991f65bf81
yangjb111 <yangjb111@gmail.com>
2018-08-12 08:16:40 +0800

    add README; using ajax request in main.js; finish the todo app

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to database
+mongoose.connect('mongodb://127.0.0.1:27017/my_database',  { useNewUrlParser: true });
+
+mongoose.connection.once('open', function(){
+  console.log('connect to database successfully');
+});
+
+// define a model
+var Schema = mongoose.Schema;
+var TodoSchema = new Schema({
+  item: String
+});
+
+var TodoModel = mongoose.model('Todos', TodoSchema);
+
+// const item = new TodoModel();
+// item.item = "sleeping 8 hours";
+// item.save(function(err){
+//   if (err) throw err;
+//   console.log('save item ok: ', item.item);
+//
+// });
+
+////testing, remove database
+// TodoModel.remove({}, function(){
+//   console.log("remove database");
+// });
 
 // use local array to papulate the todo list, testing ejs engine
-var todos = ['drinking some water', 'play basketball', 'buy some flower'];
-
-var simple = 'hello ejs';
-
+var todos = [{item:"test"}];
 
 
 // create express app
@@ -21,24 +47,47 @@ app.set('view engine', 'ejs');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // set router
-app.get('/', function(req, res){
+app.get('/todo', function(req, res){
   // res.sendFile(__dirname + '/todo.html');
-  res.render('todo', {data: todos}); // default to views folder
+
+  TodoModel.find({}, function (err, docs) {
+    if(err) throw err;
+    // docs is an array
+    res.render('todo', {data: docs}); // default to views folder
+  });
 });
 
 //use middle-ware to parse post request
-app.post('/', urlencodedParser, function(req, res){
-  console.log(req.body.new_item);
+app.post('/todo', urlencodedParser, function(req, res){
+  // console.log(req.body.new_item);
+
+  // add item to mongoDB
+  var item = TodoModel(req.body).save(function(err, docs){
+    if (err) throw err;
+    res.json(docs);
+  });
 
   // res.send('you add new item successfully' + req.body.new_item)
-  todos.push(req.body.new_item);
-  res.render('todo', {data: todos});
+  // todos.push(req.body.new_item);
+
+});
+
+app.delete('/todo/:item', function(req, res){
+  console.log('recv a delete request for:', req.params.item);
+  var item = req.params.item.replace(/\-/g, " ")
+  TodoModel.find({item: item}).remove(function(err, docs){
+    if(err) throw err;
+    res.json(docs);
+  });
+
+  // todos.filter(forEach(function(item){}))
+  // res.render('todo', {data: todos});
 });
 
 // just for testing, should be removed when app is completed
-app.get('/index', function(req, res){
-  res.sendFile(__dirname + '/todo.html')
-});
+// app.get('/index', function(req, res){
+//   res.sendFile(__dirname + '/todo.html')
+// });
 
 // listen
 app.listen(3000);
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,35 @@
+$(document).ready(function(){
 
+  $('#item-form').on('submit', function(){
+    console.log('submit ...');
+
+    var item = $('#new-item'); // the selector must be write
+    var todo = {item: item.val()};
+    // console.log(todo);
+    $.ajax({
+      url: '/todo',
+      type: 'POST',
+      data: todo,
+      success: function(data){
+        console.log(data);
+        location.reload();
+      }
+    });
+    return false;
+  });
+
+  $("li").on('click', function(event){
+    // event.preventDefault();
+    var item = $(this).text().replace(/ /g, "-");
+    console.log(item, "is about to delete");
+    $.ajax({
+      url: '/todo/' + item,
+      type: 'DELETE',
+      success: function(result){
+        console.log('ajax request send ok, and receive the correct response');
+        location.reload();
+      }
+    });
+  });
+
+});
